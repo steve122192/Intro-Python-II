7f7da501dbd3bd4ebc410e0e71f79736129a96ff
Steven Reiss <sgreiss92@gmail.com>
2020-06-04 22:46:44 -0400

    day 2 MVP

--- src/adv.py
+++ src/adv.py
@@ -1,25 +1,29 @@
 from room import Room
 from player import Player
+from item import Item
 
 # Declare all the rooms
 
 room = {
     'outside':  Room("Outside Cave Entrance",
-                     "North of you, the cave mouth beckons"),
+                     "North of you, the cave mouth beckons",
+                     [Item('Torch','light your way')]),
 
     'foyer':    Room("Foyer", """Dim light filters in from the south. Dusty
 passages run north and east."""),
 
     'overlook': Room("Grand Overlook", """A steep cliff appears before you, falling
 into the darkness. Ahead to the north, a light flickers in
-the distance, but there is no way across the chasm."""),
+the distance, but there is no way across the chasm.""",
+[Item('Bread','gain energy'), Item('Meat','build strength')]),
 
     'narrow':   Room("Narrow Passage", """The narrow passage bends here from west
 to north. The smell of gold permeates the air."""),
 
     'treasure': Room("Treasure Chamber", """You've found the long-lost treasure
 chamber! Sadly, it has already been completely emptied by
-earlier adventurers. The only exit is to the south."""),
+earlier adventurers. The only exit is to the south.""",
+[Item('Coin',' buy items')])
 }
 
 
@@ -46,6 +50,57 @@ player1 = Player('Steve', room['outside'])
 while True:
     print(player1.current_room.name)
     print(player1.current_room.description)
+    if len(player1.current_room.items) != 0:
+        items_list = player1.current_room.items.copy()
+        for item in items_list:
+            good_answer = False
+            print(f"You Found:\n{item.name} to {item.description}")
+            while good_answer is False:
+                ans = input('Take Item?(y/n):')
+                if ans == 'y':
+                    player1.items.append(item)
+                    player1.current_room.items.remove(item)
+                    good_answer = True
+                elif ans == 'n':
+                    good_answer = True
+                else:
+                    print('Choose yes or no!')
+
+        
+        items_list = []
+        for item in player1.items:
+            items_list.append(item.name)
+        print(f'Your Inventory:{str(items_list)[1:-1]}')
+
+        good_answer = False
+        while good_answer is False:
+            ans = input('Manage Inventory?(y/n):')
+            if ans == 'y':
+                good_answer = True
+                items_list = player1.items.copy()
+                for item in items_list:
+                    rem = input(f'Remove {item.name}?(y/n)')
+                    if rem == 'y':
+                        player1.items.remove(item)
+                        player1.current_room.items.append(item)
+                        item_list = []
+                        for item in player1.items:
+                            item_list.append(item.name)
+                        print(f'Your Inventory:{str(item_list)[1:-1]}')
+                    elif rem == 'n':
+                        pass
+                    else:
+                        pass
+            elif ans == 'n':
+                good_answer = True
+            else:
+                print('Choose yes or no!')
+
+
+
+
+    else:
+        print('No itmes to be found in this room...')
     direction = input('Which direction do you move?(n,s,e,w):')
     if direction == 'n':
         try:
@@ -66,7 +121,12 @@ while True:
         try:
             player1.current_room = player1.current_room.w_to
         except:
-            print('You can not move in this direction!') 
+            print('You can not move in this direction!')
+    if direction == "inventory":
+        item_list = []
+        for item in player1.items:
+            item_list.append(item.name)
+        print(f'Your Inventory:{str(item_list)[1:-1]}')
     if direction == 'q':
         break 
 
